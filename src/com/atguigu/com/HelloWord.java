9fa11a3df728c5fd53d6b20307afe0b695a2d97d
bao-jian-zhong <Bao_Jian_Zhong@163.com>
2021-05-02 17:38:13 +0800

    Initial commit

---
+++ src/com/atguigu/com/HelloWord.java
@@ -0,0 +1,18 @@
+package com.atguigu.com;
+
+import java.util.ArrayList;
+
+public class HelloWord {
+    //单行注释
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Word!!");
+
+        ArrayList arr=new ArrayList();
+    }
+
+    public void method(){
+
+
+    }
+}
